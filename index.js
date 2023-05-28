be7b786dd394e412f43e518a6390e5fd990209c2
Arunjith T S <arunjith.unofficial@gmail.com>
2023-05-28 12:59:32 +0530

    added instagram download method and a logic to select the method based on the platform

--- index.js
+++ index.js
@@ -2,17 +2,54 @@ const express = require("express");
 const app = express();
 const ytdl = require("ytdl-core");
 
-app.get("/download", (req, res) => {
-  const videoUrl = req.query.url; // get from query parameter
-  if (!videoUrl) {
-    return res.status(400).json({ error: "video link is required" });
-  }
+app.get("/download", async (req, res) => {
+  const videoUrl = req.query.url;
+  const platform = req.query.platform;
 
-  ytdl(videoUrl).pipe(res); // stream video as response
+  if (!videoUrl || !platform) {
+    return res
+      .status(400)
+      .json({ error: "video link and platform is required" });
+  }
+  switch (platform) {
+    case "instagram":
+      try {
+        //fetching content of the post (html)
+        const response = await axios.get(videoUrl);
+        const html = response.data;
+
+        //parsing html content using cheerio
+        const $ = cheerio.load(html);
+
+        //extract video URL from the html
+        const videoSrc = $(`meta[property="og:video"]`).attr("content");
+        if (!videoSrc) {
+          return res
+            .status(400)
+            .json({ error: "video url is not present in this post" });
+        }
+        // Downloading the video
+        const videoResponse = await axios.get(videoSrc, {
+          responseType: "stream",
+        });
+        videoResponse.data.pipe(res); //stream the videoResponse
+      } catch (error) {
+        console.log("Error:", error);
+        res
+          .status(500)
+          .json({ error: "There was an error when downloading the video" });
+      }
+      break;
+    case "youtube":
+      ytdl(videoUrl).pipe(res); // stream video as response
+      break;
+    default:
+      return res.status(400).json("invalid platform selected");
+  }
 });
 
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+  console.log(`server is running on port ${port}`);
+});
